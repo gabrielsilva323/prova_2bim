ea2a6f372024de031d4590fd9c9a9115b04a2c76
gabrielsilva323 <143452553+gabrielsilva323@users.noreply.github.com>
2024-05-15 18:18:59 -0300

    Add files via upload

---
+++ prova_finalizada.sql
@@ -0,0 +1,12 @@
+use avaliacao_22b;
+ -- 1.insert into Livros (titulo, autor, ano_publicacao, disponivel, categoria, isbn, editora, quantidade_paginas, idioma)
+ values ("as cronicas de narnia", "C.S Lewis", 1950, true, "fantasia", "isbn_978-0064471190", "hapercollins", 768, "ingles"); 
+ select * from Livros /*
+ -- 3.update Livros set editora = "plume books" where titulo = "1984"; 
+ -- 5.select * from Livros where quantidade_paginas > 650; 
+ -- 6.select count(categoria) 
+  from Livros group by categoria; /*
+ -- 7.select * from Livros limit 0,5; 
+ -- 8.select avg(ano_publicacao) from Livros; 
+ -- 9.select * from Livros order by ano_publicacao desc; 
+ -- 10.select * from Livros where titulo
